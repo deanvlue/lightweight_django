89f212bd2051aea82389bc585bafc6e5d711f111
deanvlue <munoz.josecarlos@gmail.com>
2015-02-26 16:30:34 -0600

    Added environment config

--- hello.py
+++ hello.py
@@ -1,22 +1,31 @@
 #!/usr/bin/python
+import os
 import sys
 
 from django.conf import settings
 
-from django.http import HttpResponse
-from django.conf.urls import url
-from django.core.wsgi import get_wsgi_application
+DEBUG = os.environ.get('DEBUG','on') == 'on'
+SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(32))
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS','localhost').split(',')
+
 
 settings.configure(
-        DEBUG = True,
-        SECRET_KEY = 'wawaweweririroeoeir',
+        DEBUG = DEBUG,
+        SECRET_KEY = SECRET_KEY,
+        ALLOWED_HOSTS = ALLOWED_HOSTS,
         ROOT_URLCONF = __name__,
+
         MIDDLEWARE_CLASSES = (
             'django.middleware.common.CommonMiddleware',
             'django.middleware.csrf.CsrfViewMiddleware',
             'django.middleware.clickjacking.XFrameOptionsMiddleware',
             ),
         )
+
+from django.http import HttpResponse
+from django.conf.urls import url
+from django.core.wsgi import get_wsgi_application
+
 
 application = get_wsgi_application()
 
