fb79623798f7695b362b07a3906c6efdec5dec43
deanvlue <munoz.josecarlos@gmail.com>
2015-02-26 16:15:52 -0600

    first commit

---
+++ hello.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+import sys
+
+from django.conf import settings
+
+from django.http import HttpResponse
+from django.conf.urls import url
+from django.core.wsgi import get_wsgi_application
+
+settings.configure(
+        DEBUG = True,
+        SECRET_KEY = 'wawaweweririroeoeir',
+        ROOT_URLCONF = __name__,
+        MIDDLEWARE_CLASSES = (
+            'django.middleware.common.CommonMiddleware',
+            'django.middleware.csrf.CsrfViewMiddleware',
+            'django.middleware.clickjacking.XFrameOptionsMiddleware',
+            ),
+        )
+
+application = get_wsgi_application()
+
+def index(request):
+    return HttpResponse("Hello World")
+
+urlpatterns={
+        url(r'^$', index)
+        }
+
+if __name__ == "__main__":
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
