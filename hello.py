f85ca9d23584cacd139b02948d01bae6c243ee5d
deanvlue <munoz.josecarlos@gmail.com>
2015-02-26 18:52:04 -0600

    Added mxnbtc definition

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
+from __future__ import division
 import os
 import sys
+import json
 
 from django.conf import settings
 
@@ -23,18 +25,26 @@ settings.configure(
         )
 
 from django.http import HttpResponse
-from django.conf.urls import url
+from django.conf.urls import url, patterns
 from django.core.wsgi import get_wsgi_application
-
 
 application = get_wsgi_application()
 
 def index(request):
     return HttpResponse("Hello World")
 
-urlpatterns={
-        url(r'^$', index)
-        }
+def btcmxn(request):
+    btc = 3000
+    mxn = 1/3000
+    quote = {"MXN":mxn,"BTC":btc}
+    quote_json = json.dumps(quote)
+    return HttpResponse(quote_json)
+
+urlpatterns= patterns('',
+        url(r'^$', index),
+        url(r'^hello/$', btcmxn),
+        
+        )
 
 if __name__ == "__main__":
     from django.core.management import execute_from_command_line
